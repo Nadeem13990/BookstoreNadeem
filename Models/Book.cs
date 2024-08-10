fb73ba9c6992d5497cbd843452c5e593c7df1c40
Nadeem13990 <5355270@herzing.ca>
2024-08-10 17:31:35 -0400

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookstore.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+
+}
