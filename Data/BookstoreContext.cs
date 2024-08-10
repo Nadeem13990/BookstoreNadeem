8302a5c8b6543a11be1d5e5f307b99abcdb01a00
Nadeem13990 <5355270@herzing.ca>
2024-08-10 17:38:55 -0400

    Add files via upload

---
+++ Data/BookstoreContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookstore.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Bookstore.Models;
+    using Microsoft.AspNetCore.Identity;
+
+    public class BookstoreContext : IdentityDbContext<IdentityUser>
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
